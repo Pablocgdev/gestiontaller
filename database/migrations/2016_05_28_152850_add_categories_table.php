df1bc79c6c1749eb50b2b8c5137532af18e54430
Pablocgdev <pablocgdev@gmail.com>
2016-05-28 17:36:49 +0200

    Added migrations for users, passwords resets and categories

--- database/migrations/2016_05_28_152850_add_categories_table.php
+++ database/migrations/2016_05_28_152850_add_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class AddCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60);
+            $table->timestamps();
         });
     }
-    
+
     /**
      * Reverse the migrations.
      *
@@ -26,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::drop('categories');
     }
 }
