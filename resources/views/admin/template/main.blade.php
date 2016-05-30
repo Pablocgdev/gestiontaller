e9c3f37430d5ecd4e801dfe0794861a489fe22b5
Pablocgdev <pablocgdev@gmail.com>
2016-05-30 21:45:10 +0200

    Minor styles changes

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>@yield('title', 'Default') | Admin Panel</title>
 	<link rel="stylesheet" href="{{asset('plugins/bootstrap/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('css/main.css')}}">
 </head>
 <body>	
 	@include('admin.template.partials.nav')
@@ -13,7 +14,7 @@
 		@include('admin.template.partials.errors')
 		@yield('content')
 	</section>
-
+	@include('admin.template.partials.footer')
 	<script src="{{asset('plugins/jquery/jquery-2.2.4.js')}}"></script>
 	<script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
 </body>
---
+++ resources/views/admin/template/partials/footer.blade.php
@@ -0,0 +1,6 @@
+
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Está logeado como {{$user->type}}</p>
+      </div>
+    </footer>
