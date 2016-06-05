16ee1f25effb5ce71497a9d2f72777cc8af4b514
Pablocgdev <pablocgdev@gmail.com>
2016-06-05 21:31:01 +0200

    Fixed typo

--- resources/views/admin/template/partials/nav.blade.php
+++ resources/views/admin/template/partials/nav.blade.php
@@ -19,7 +19,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">
-    <li><a href="{{ route('admin.auth.login') }}">Login.</a></li>
+    <li><a href="{{ route('admin.auth.login') }}">Login</a></li>
     </ul>
     @if(Auth::user())
 
